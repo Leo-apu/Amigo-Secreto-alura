74740a7cffbed5fe7ea236c4b9ae570047b4774d
Leo-apu <34524268@fi.unju.edu.ar>
2025-01-17 00:57:51 -0300

    add array friends

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const listaAmigos = document.getElementById("listaAmigos");
 const boton = document.querySelector(".button-draw");
+const amigosArray = [];
 
 estadoBoton();
 function estadoBoton() {
-    if (listaAmigos.children.length < 2) {
+    if (amigosArray.length < 2) {
         boton.disabled = true;
     } else {
         boton.disabled = false;
@@ -18,27 +19,40 @@ function agregarAmigo() {
         return;
     }
 
+    amigosArray.push(amigo);
+
     const amigos = document.getElementById("listaAmigos");
     const amigoItem = document.createElement("li");
     amigoItem.textContent = amigo;
     amigos.appendChild(amigoItem);
     document.getElementById("amigo").value = "";
     estadoBoton();
+    console.log(amigosArray);
 }
 
 
 
 function sortearAmigo() {
-    const amigos = document.querySelectorAll("#listaAmigos li");
+    /* const amigos = document.querySelectorAll("#listaAmigos li");
     const amigoSecreto = amigos[Math.floor(Math.random() * amigos.length)];
     const resultado = document.getElementById("resultado");
-    resultado.textContent = "El amigo secreto es: " + amigoSecreto.textContent;
+    resultado.textContent = "El amigo secreto es: " + amigoSecreto.textContent; */
 
-    const listaAmigos = document.getElementById("listaAmigos");
+    /* const listaAmigos = document.getElementById("listaAmigos");
+    while (listaAmigos.firstChild) {
+        listaAmigos.removeChild(listaAmigos.firstChild);
+    } */
+
+    const amigoSecreto = amigosArray[Math.floor(Math.random() * amigosArray.length)];
+    const resultado = document.getElementById("resultado");
+    resultado.textContent = "El amigo secreto es: " + amigoSecreto;
+
+    // Limpiamos la lista HTML, pero mantenemos el array intacto
     while (listaAmigos.firstChild) {
         listaAmigos.removeChild(listaAmigos.firstChild);
     }
 
+    amigosArray.length = 0; 
     estadoBoton();
 }
 
